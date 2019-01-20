961ddb6a0c1fd5faa60447468658cb379bf30a9a
Raúl Enrique Reza del Castillo <32489142+BiggerCrayonZ@users.noreply.github.com>
2019-01-20 03:48:46 -0600

    login ready and main router done

--- eventos-ui/src/App.js
+++ eventos-ui/src/App.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+
+/* Components */
+import Navbar from './components/template/NavBar';
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
+import Login from './components/auth/Login';
+
+
+const config = {
+  issuer: 'https://dev-917519.oktapreview.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oaj1kv24yYQSExTt0h7'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Security issuer={config.issuer}
+          client_id={config.client_id}
+          redirect_uri={config.redirect_uri}
+        >
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Route path="/" exact={true} component={Home} />
+              <SecureRoute path="/staff" exact={true} component={Staff} />
+              <Route
+                path="/login"
+                render={() => (
+                  < Login baseUrl='https://dev-917519.oktapreview.com/oauth2/default' />
+                )} />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+            </div>
+          </div>
+        </Security>
+      </Router>
     );
   }
 }
---
+++ eventos-ui/src/components/template/NavBar.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem
+} from 'reactstrap';
+
+class NavBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = { isOpen: false };
+    }
+    toggle() { this.setState({ isOpen: !this.state.isOpen }); }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="sm">
+                    <NavbarBrand href="/">Gestor de Eventos</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem> <Link className="nav-link" to="/"> Home </Link> </NavItem>
+                            <NavItem> <Link className="nav-link" to="/staff"> Staff </Link> </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        )
+    }
+}
+
+export default NavBar;
